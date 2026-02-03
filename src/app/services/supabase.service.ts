6dc23d9125d6b93b0f54bcce4895b457e8366ba7
Abhay433 <abhayn433@gmail.com>
2026-02-03 14:53:15 +0530

    check

---
+++ src/app/services/supabase.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(
+      'https://gtpljesgoyaumleugwrs.supabase.co',   // ✅ your real Supabase URL
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0cGxqZXNnb3lhdW1sZXVnd3JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwMjM1MDQsImV4cCI6MjA4NTU5OTUwNH0.XYtuSmd9P7UQ6DgmjaXlLmvgSlpptwa4xs-ksG8qlwc'                   // ✅ rotated key
+    );
+  }
+
+  async saveContact(formData: any) {
+    return await this.supabase
+      .from('contact_messages')
+      .insert([formData]);
+  }
+}
